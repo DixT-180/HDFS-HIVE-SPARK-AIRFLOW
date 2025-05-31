a8a0e56bfc01d6836b34a38e1a42135cb344d593
dikshyant thapa <dikshyant180@gmail.com>
2025-05-31 20:11:12 +0545

    hdfs-spark-hiveMetastore integrated

---
+++ spark-jobs/read_people_csv.py
@@ -0,0 +1,44 @@
+import sys
+import boto3
+import pandas as pd
+import io
+from pyspark.sql import SparkSession
+
+
+spark = (
+    SparkSession.builder
+    .appName("WriteToHDFS")
+    .config("spark.driver.memory", "2g")         # 4 GB RAM for the driver
+    .config("spark.executor.memory", "2g")       # 4 GB RAM per executor
+    .config("spark.executor.cores", "4")         # 4 cores per executor
+    .config("spark.cores.max", "4")              # Max total cores
+    .getOrCreate()
+)
+
+# Args
+bucket = sys.argv[1] if len(sys.argv) > 1 else "mybucket"
+filename = sys.argv[2] if len(sys.argv) > 2 else "people.csv"
+hdfs_target = "hdfs://namenode:9000/user/staging_area/" #+ filename
+
+# Get data from MinIO with boto3
+s3 = boto3.client(
+    's3',
+    endpoint_url='http://minio:9003',
+    aws_access_key_id='minioadmin',
+    aws_secret_access_key='minioadmin',
+    region_name='us-east-1',
+)
+obj = s3.get_object(Bucket=bucket, Key=filename)
+df_pd = pd.read_csv(io.BytesIO(obj['Body'].read()))
+
+# Create Spark session
+spark = SparkSession.builder.appName("WriteToHDFS").getOrCreate()
+
+# Convert pandas DataFrame to Spark DataFrame
+df_spark = spark.createDataFrame(df_pd)
+
+# Save as CSV to HDFS (can use .parquet, .json, .orc, etc.)
+df_spark.write.mode("overwrite").csv(hdfs_target, header=True)
+print(f"✅ Saved to HDFS: {hdfs_target}")
+
+spark.stop()
---
+++ spark-jobs/stream_to_iceberg.py
@@ -0,0 +1,69 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from pyspark.sql.functions import col, lower, when
+from pyspark.sql.functions import col
+
+schema = StructType([
+    StructField("id", IntegerType()),
+    StructField("name", StringType()),
+    StructField("age", IntegerType()),
+    StructField("gender", StringType())
+])
+
+spark = SparkSession.builder \
+    .appName("stream-csv-to-iceberg") \
+    .config("spark.executor.cores", "2") \
+    .config("spark.executor.memory", "2g") \
+    .config("spark.cores.max", "6") \
+    .config("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog") \
+    .config("spark.sql.catalog.local.type", "hadoop") \
+    .config("spark.sql.catalog.local.warehouse", "hdfs://namenode:9000/warehouse/iceberg") \
+    .getOrCreate()
+
+
+
+# .appName("stream-csv-to-iceberg") \
+#     .config("spark.executor.cores", "2") \
+#     .config("spark.executor.memory", "1g") \
+#     .config("spark.cores.max", "6") \
+
+df = spark.readStream \
+    .option("header", True) \
+    .schema(schema) \
+    .csv("hdfs://namenode:9000/user/staging_area/")
+
+
+
+df = df \
+    .filter(col("age") >= 25) \
+    .withColumn("gender", lower(col("gender"))) \
+    .withColumn(
+        "age",
+        when(col("age") < 35, "young")
+        .when((col("age") >= 35) & (col("age") < 50), "middle-aged")
+        .otherwise("senior")
+    )
+
+# Create the database if it doesn't exist
+# spark.sql("DROP TABLE IF EXISTS local.people_db.people")
+
+spark.sql("CREATE DATABASE IF NOT EXISTS local.people_db")
+
+# Create Iceberg table if not exists (schema must match your data)
+spark.sql("""
+    CREATE TABLE IF NOT EXISTS local.people_db.people (
+      id INT,
+      name STRING,
+      age STRING,
+      gender STRING
+    ) USING ICEBERG
+""")
+
+query = df.writeStream \
+    .format("iceberg") \
+    .outputMode("append") \
+    .option("checkpointLocation", "hdfs://namenode:9000/user/streaming_checkpoint/people") \
+    .trigger(processingTime="10 seconds") \
+    .toTable("local.people_db.people")
+
+query.awaitTermination()
